0a3094b912cfeb053f6c05305fc6b763041a7867
mikeshaggy <michalbagan@icloud.com>
2023-11-20 23:11:05 +0100

    Implemented functionalities to get details about specific portfolio, such as total estimated value etc.

--- CryptoTracker/src/main/java/Portfolio.java
+++ CryptoTracker/src/main/java/Portfolio.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Portfolio implements Serializable {
     private String name;
     private List<Trade> trades = new ArrayList<>();
-    private Map<String, Double> coinsQuantity = new HashMap<>();
+    private Map<String, Double> content = new HashMap<>();
     private double investedAmount;
 
     public Portfolio(String name) {
@@ -16,13 +16,13 @@ public class Portfolio implements Serializable {
 
     public void addTrade(Trade trade) {
         trades.add(trade);
-        investedAmount += trade.getPrice();
         String coinName = trade.getCoinName();
         double coinQuantity = trade.getQuantity();
-        if (coinsQuantity.containsKey(coinName)) {
-            coinsQuantity.put(coinName, coinsQuantity.get(coinName) + coinQuantity);
+        investedAmount += trade.getPrice() * coinQuantity;
+        if (content.containsKey(coinName)) {
+            content.put(coinName, content.get(coinName) + coinQuantity);
         } else {
-            coinsQuantity.put(coinName, coinQuantity);
+            content.put(coinName, coinQuantity);
         }
     }
 
@@ -36,8 +36,45 @@ public class Portfolio implements Serializable {
         System.out.println("Trade list is empty");
     }
 
-    public Map<String, Double> getCoinsQuantity() {
-        return coinsQuantity;
+    private void getContent() {
+        System.out.printf("%-5s | %-5s | %-15s%n", "Coin Name", "Held Quantity", "Estimated Value in USD [$]");
+        for (Map.Entry<String, Double> entry : content.entrySet()) {
+            String coinName = entry.getKey();
+            double coinAmount = entry.getValue();
+            double coinPrice = CoinAPI.getCoinExchangeRate(coinName);
+            double coinValue = coinPrice * coinAmount;
+            System.out.printf("%-9s | %-13.9f | %-15.2f%n", coinName, coinAmount, coinValue);
+        }
+    }
+
+    private double getValue() {
+        double value = 0;
+        for (Map.Entry<String, Double> entry : content.entrySet()) {
+            String coinName = entry.getKey();
+            double coinAmount = entry.getValue();
+            double coinPrice = CoinAPI.getCoinExchangeRate(coinName);
+            value += coinPrice * coinAmount;
+        }
+
+        return value;
+    }
+
+    private double getProfit() {
+        double currentValue = getValue();
+
+        return ((currentValue - investedAmount) / investedAmount) * 100;
+    }
+
+    public void getDetails() {
+        double profit = getProfit();
+
+        String result = profit >= 0 ? "Gain" : "Loss";
+
+        getContent();
+        System.out.println("-".repeat(60));
+        System.out.printf("Invested amount: " + investedAmount + "$");
+        System.out.printf("Total portfolio value: %.2f$%n", getValue());
+        System.out.printf("%s: %.2f%%%n", result, profit);
     }
 
     public String getName() {
